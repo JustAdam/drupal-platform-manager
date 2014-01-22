a788d83eeaf71188839cd292e99cca83f471c41b
JustAdam <adambell7@gmail.com>
2014-01-22 14:54:39 +0100

    Moved asset types variable to parent class.

--- src/Dbmedialab/Drupal/Deploy/Modulefetch/ModuleFetchCommand.php
+++ src/Dbmedialab/Drupal/Deploy/Modulefetch/ModuleFetchCommand.php
@@ -6,6 +6,9 @@ use Symfony\Component\Console\Command\Command;
 
 abstract class ModuleFetchCommand extends Command {
 
+  protected $asset_types = ['modules', 'libraries', 'themes'];
+  
+
   public function __call($name, $args) {
     return call_user_func_array(array($this->getApplication(), $name), $args);
   }
